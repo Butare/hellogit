48a40b12272b064776ba9235485c6f9cdce130e5
Butare <bjimsons@gmail.com>
2016-12-04 18:17:44 +0900

    Call function addede

--- HelloGit/src/jim/test/git/HelloGit.java
+++ HelloGit/src/jim/test/git/HelloGit.java
@@ -4,17 +4,18 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// greeting okay
-		System.out.println("Hello, Git!");
+		System.out.println("Value :"+sqrt(4.0));
+		
 	}
 	
 	// newton's square root calculation
 	public static double sqrt(double n){
 		
 		double c = n;    	// current estimate
-		double er = 1e-13; 	// desired precision
+		double er = 1e-16; 	// desired precision
 		
 		while ( Math.abs(c - n/c) > er * c)
-			c = (n/c + c) / 2.0;
+			c = (n / c + c) / 2.0;
 		
 		return c;
 	}
